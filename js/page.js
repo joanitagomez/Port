d5a8c7760261e80c8b807cd6c6743b301eef51ec
joanitagomez@gmail.com <joanitagomez@gmail.com>
2017-07-31 22:33:31 -0700

    Improved responsiveness

--- js/page.js
+++ js/page.js
@@ -2,20 +2,67 @@ $(document).ready(function() {
 
   $('.title-container').velocity("transition.slideDownBigIn");
 
+  $('.img-container').velocity("transition.slideDownBigIn");
+  $('.about-text').velocity("transition.slideDownBigIn", {
+    display: "flex"
+  });
   var state = "closed";
 
   function closeNavbar() {
-    $('.menu-overlay').velocity('transition.slideRightBigOut');
+
+    $(".menu").velocity("transition.fadeOut", {
+      duration: 300
+    });
+    $(".menu-overlay").velocity({
+      width: "0%"
+    }, {
+      duration: 300
+    });
     state = "closed";
   }
-  $('.navigation').on('click', function() {
+
+  $(window).on("scroll", function() {
+    if (($(window).scrollTop() > 780) && ($(window).scrollTop() < 2700)) {
+      $(".side-bar").addClass("on-blue");
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      $(".side-bar").removeClass("on-blue");
+    }
+  });
+
+  $('.nav-icon').on('click', function(event) {
+    event.stopPropagation();
+
+    var handle = function() {
+      closeNavbar();
+      $("body").off(".validator");
+    };
+
+
     if (state === "closed") {
-      $('.menu-overlay').velocity('transition.slideRightBigIn');
+
+
+      if ($(window).width() <= 768) {
+        $('.menu-overlay').velocity({
+          width: "80%"
+        });
+      } else {
+        $('.menu-overlay').velocity({
+          width: "30%"
+        });
+      }
+      $(".menu").velocity("transition.fadeIn");
+
+      $('body').on("click.validator", handle);
       state = "open";
+
     } else {
       closeNavbar();
     }
   });
+
+
+
   $('.home').on("click", function() {
     closeNavbar();
     $('html, body').animate({
@@ -31,6 +78,14 @@ $(document).ready(function() {
     }, 1000);
 
   });
+  $('.say-hello').on("click", function() {
+    closeNavbar();
+    $('html, body').animate({
+      scrollTop: $(".contact-page").offset().top
+    }, 1000);
+  });
+
+
   $('.say-hello').on("click", function() {
     closeNavbar();
     $('html, body').animate({
