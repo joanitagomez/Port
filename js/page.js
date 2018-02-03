80a7dd2cbae1d3137a1b057c8c587c025cd2a813
joanitagomez@gmail.com <joanitagomez@gmail.com>
2018-02-02 17:51:05 -0800

    New version

--- js/page.js
+++ js/page.js
@@ -1,94 +1,51 @@
 $(document).ready(function() {
-
-  $('.title-container').velocity("transition.slideDownBigIn");
-
-  $('.img-container').velocity("transition.slideDownBigIn");
-  $('.about-text').velocity("transition.slideDownBigIn", {
-    display: "flex"
-  });
-  var state = "closed";
-
-  function closeNavbar() {
-
-    $(".menu").velocity("transition.fadeOut", {
-      duration: 300
-    });
-    $(".menu-overlay").velocity({
-      width: "0%"
-    }, {
-      duration: 300
-    });
-    state = "closed";
-  }
-
+ 
   $(window).on("scroll", function() {
-    if (($(window).scrollTop() > 650) && ($(window).scrollTop() < 2700)) {
-      $(".side-bar").addClass("on-blue");
+    if ($(window).scrollTop() > 100) {
+      $(".nav-bar").addClass("shrink");
     } else {
-      $(".side-bar").removeClass("on-blue");
+      $(".nav-bar").removeClass("shrink");
     }
   });
 
-  $('.nav-icon').on('click', function(event) {
-    event.stopPropagation();
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
 
-    var handle = function() {
-      closeNavbar();
-      $("body").off(".validator");
-    };
+$(window).scroll(function(event){
+    didScroll = true;
+});
 
-
-    if (state === "closed") {
-
-
-      if ($(window).width() <= 768) {
-        $('.menu-overlay').velocity({
-          width: "80%"
-        });
-      } else {
-        $('.menu-overlay').velocity({
-          width: "30%"
-        });
-      }
-      $(".menu").velocity("transition.fadeIn");
-
-      $('body').on("click.validator", handle);
-      state = "open";
-
-    } else {
-      closeNavbar();
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
     }
-  });
+}, 20);
 
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
 
+        }
+    }
+    
+    lastScrollTop = st;
+}
 
-  $('.home').on("click", function() {
-    closeNavbar();
-    $('html, body').animate({
-      scrollTop: $(".title-container").offset().top
-    }, 1000);
-  });
-
-  $('.work').on("click", function() {
-    closeNavbar();
-
-    $('html, body').animate({
-      scrollTop: $(".work-container").offset().top
-    }, 1000);
-
-  });
-  $('.say-hello').on("click", function() {
-    closeNavbar();
-    $('html, body').animate({
-      scrollTop: $(".contact-page").offset().top
-    }, 1000);
-  });
-
-
-  $('.say-hello').on("click", function() {
-    closeNavbar();
-    $('html, body').animate({
-      scrollTop: $(".contact-page").offset().top
-    }, 1000);
-  });
 });
